b434ba862f1b2cb86cb0d5d7b56b49e3a763f0a7
Janaina Mota <janaother16@gmail.com>
2024-10-08 18:40:16 -0300

    atualização do token e add de jar

--- src/main/java/com/partiutech/apichatbot/whatsapp/util/WhatsappUtil.java
+++ src/main/java/com/partiutech/apichatbot/whatsapp/util/WhatsappUtil.java
@@ -20,7 +20,7 @@ public class WhatsappUtil {
         GenericUrl genericUrl = new GenericUrl(urlAPI);
         HttpRequest request = httpRequestFactory.buildPostRequest(genericUrl, ByteArrayContent.fromString(null, conteudo));
 
-        request.getHeaders().setAuthorization("Bearer EAAXLmAKvZBrwBO8rnSwMUVres74ntspxwx9ZAdbWyEGAtZBl3ZCvwgGGSInaW6zDG8f3gGpeW8nMe4kZAM0pUZCSZCh98DMPGXYde1kWLEO9NYGz053CZA47Ib9fmBovbKrYruimL2nzIBjal0glhuH9tcZAVWDYtgaab57viJerwAZB8ZCTkQ5lK6wdJcr16QxvJxZCZB5slHjQFDF9HUGJkzPt3kFXrhTYZD");
+        request.getHeaders().setAuthorization("Bearer EAAXLmAKvZBrwBOwO6Gcom57URuolr6VWVvTXzE6NIyv5ZAtHL7WLGRl3hHuxXmIyTk6Ha7ozAVWZBsRoOdDGTW2lqTZAj4bRZAr2SbcrchCSzEwPhD5NAzBMgQoKrHs0OJ08ZCY5bsNEYlTE7kgbTUZCQmq8QpZAyF1hmOlOUPvMjzqNP9surwkFSVcm4SZBe7EGqUwZDZD");
 
         HttpHeaders httpHeaders = request.getHeaders();
         request.getHeaders().setContentType(APPLICATION_JSON.toString());
